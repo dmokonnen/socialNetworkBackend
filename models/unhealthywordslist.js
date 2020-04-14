c27c7943bb5975ca03490646b66be711c395ede6
MenilikBelay <menilikbelay@gmail.com>
2020-04-14 12:05:14 -0500

    CRUD for unhealthywords done

--- models/unhealthywordslist.js
+++ models/unhealthywordslist.js
@@ -1,23 +1,37 @@
-const Joi = require('joi');
-const mongoose = require('mongoose');
+const Joi = require("joi");
+const mongoose = require("mongoose");
 
 const unhealthyWordsSchema = new mongoose.Schema({
-
-  wordlist: [{
-    type: String, 
-    require:true,  
-    unique:true 
-  }]  
+  word: {
+    type: String,
+    require: true,
+    unique: true,
+  },
 });
 
-const UnhealthyWords = mongoose.model('UnhealthyWords', unhealthyWordsSchema);
+/**
+ * Custom error generator for duplicates
+ */
+const modelErrorHandler = (error, res, next) => {
+  if (error.name === "MongoError" && error.code === 11000) {
+    next({ message: "There was a duplicate key error", code: 11000 });
+  } else {
+    next(); // will still error out
+  }
+};
+
+unhealthyWordsSchema.post("save", modelErrorHandler); // for create errors
+
+unhealthyWordsSchema.post("update", modelErrorHandler); // for update errors
+
+const UnhealthyWords = mongoose.model("UnhealthyWords", unhealthyWordsSchema);
 
 function validateUnhealthy(unhealthyWords) {
   const schema = {
-    wordlist: Joi.array().min(1).max(255)
+    word: Joi.string().min(1).max(255).required(),
   };
   return Joi.validate(unhealthyWords, schema);
 }
 
-exports.UnhealthyWords = UnhealthyWords; 
+exports.UnhealthyWords = UnhealthyWords;
 exports.validateUnhealthylist = validateUnhealthy;
--- routes/admin.js
+++ routes/admin.js
@@ -1,27 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const path = require('path');
-const auth = require('../middleware/auth');
-const admin = require('../middleware/admin');
-const adminController = require('../controllers/admin');
-const uploadController = require('../controllers/upload');
+const path = require("path");
+const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
+const adminController = require("../controllers/admin");
+const uploadController = require("../controllers/upload");
 
-router.use([auth,admin]);
-router.get('/all-users',adminController.getUsers);
-router.delete('/delete-user/:id' , adminController.deleteUser);
-router.put('/deactivate-user/:id', adminController.deactivateUser);
-router.put('/activate-user/:id',adminController.activateUser);
-router.post('/push-advert',adminController.pushAdvert);
-router.get('/get-advert',adminController.getAdvert);
+router.use([auth, admin]);
+router.get("/all-users", adminController.getUsers);
+router.delete("/delete-user/:id", adminController.deleteUser);
+router.put("/deactivate-user/:id", adminController.deactivateUser);
+router.put("/activate-user/:id", adminController.activateUser);
+router.post("/push-advert", adminController.pushAdvert);
+router.get("/get-advert", adminController.getAdvert);
 router.post("/advert-images-upload", uploadController.multipleUpload);
-router.post('/create-unhealthy-word',adminController.createUnhealthyWord);
-//router.post('/add-unhealthy-word',adminController.createUnhealthyWord);
+// CRUD unhealthy words
+router.post("/unhealthyword", adminController.createUnhealthyWord);
+router.get("/unhealthywords", adminController.getUnhealthyWords);
+router.patch("/unhealthyword/:id", adminController.updateUnhealthyWord);
+router.delete("/unhealthyword/:id", adminController.deleteUnhealthyWord);
 
 //TO DO: admin post related tasks
-router.post('/post-enable',adminController.getAdvert);
-router.post('/post-disable',adminController.getAdvert);
-router.get('/unhealthy-post',adminController.getAdvert);
+router.post("/post-enable", adminController.getAdvert);
+router.post("/post-disable", adminController.getAdvert);
+router.get("/unhealthy-post", adminController.getAdvert);
 
 module.exports = router;
-
-
